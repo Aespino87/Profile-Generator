a247351811137d15d9d89e1e56ebfe5fe793c752
Adam Espino <aespino1987@gmail.com>
2020-01-03 02:40:36 -0600

    Upload of functions and api js

--- index.js
+++ index.js
@@ -1,79 +1,55 @@
-const inquirer = require("inquirer");
 const fs = require("fs");
-const util = require("util");
-
-const writeFileAsync = util.promisify(fs.writeFile);
-
-function promptUser() {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is your name?"
-    },
-    {
-      type: "input",
-      name: "location",
-      message: "Where are you from?"
-    },
-    {
-      type: "input",
-      name: "color",
-      message: "What is your favorite color"/
-    },
-    {
-      type: "input",
-      name: "food",
-      message: "What is your favorite food?"
-    },
-    {
-      type: "input",
-      name: "github",
-      message: "Enter your GitHub Username"
-    }
-    
-  ]);
-}
-
-function generateHTML(answers) {
-  return `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-    <p class="lead">I am from ${answers.location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${answers.github}</li>
-      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;
-}
-
-async function init() {
-  console.log("hi")
-  try {
-    const answers = await promptUser();
-
-    const html = generateHTML(answers);
-
-    await writeFileAsync("index.html", html);
-
-    console.log("Successfully wrote to index.html");
-  } catch(err) {
-    console.log(err);
+const inquirer = require("inquirer");
+const path = require("path");
+const open = require("open");
+const api = require("./api");
+const generateHTML = require("./generateHTML");
+const convertFactory = require("electron-html-to");
+const questions = [
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?"
+  },
+  {
+    type: "list",
+    name: "color",
+    message: "What is your favorite color?",
+    choices: ["red", "blue", "green", "pink"]
   }
+];
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
-
+function init() {
+  inquirer.prompt(questions).then(({ github, color }) => {
+    console.log("Searching...");
+    api
+      .getUser(github)
+      .then(response =>
+        api.getTotalStars(github).then(stars => {
+          return generateHTML({
+            stars,
+            color,
+            ...response.data
+          });
+        })
+      )
+      .then(html => {
+        const conversion = convertFactory({
+          converterPath: convertFactory.converters.PDF
+        });
+        conversion({ html }, function(err, result) {
+          if (err) {
+            return console.error(err);
+          }
+          result.stream.pipe(
+            fs.createWriteStream(path.join(__dirname, "resume.pdf"))
+          );
+          conversion.kill();
+        });
+        open(path.join(process.cwd(), "resume.pdf"));
+      });
+  });
+}
 init();
